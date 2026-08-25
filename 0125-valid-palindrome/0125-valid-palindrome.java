9d85a3d2d11644376eddf8b160f1d7145511403c
haricharansosukandla <haricharansosukandla@gmail.com>
2026-08-25 21:40:34 +0530

    Time: 14 ms (28.59%), Space: 46.7 MB (15.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+    int i=0;
+    
+    String s1 = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    int j=s1.length()-1;
+    while(i<j)
+    { 
+       if(s1.charAt(i)!=s1.charAt(j))
+       {
+        return false;
+        
+       }
+       i++;
+        j--;
+    }  
+    return true;
+}
+}
